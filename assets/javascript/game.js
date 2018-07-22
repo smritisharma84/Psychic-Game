1e508eaeeee6ae191b0f1da9dfd5bf248a7962d5
smritisharma84 <40214547+smritisharma84@users.noreply.github.com>
2018-07-21 23:01:57 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var winCount = 0;
     var lossCount = 0;
     var guessLeft = 9;
     var guessesEntered = []; 
-    var computerLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //list of letters to choose from
+    var computerLetter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 
     document.onkeyup = function(event) {
         var userChoice = String.fromCharCode(event.keyCode).toLowerCase(); 
